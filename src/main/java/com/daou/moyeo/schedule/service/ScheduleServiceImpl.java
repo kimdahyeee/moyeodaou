3952db781c60c5a7d71322ec61d059c6db78f27d
dahyeKim <dahye4321@naver.com>
2017-06-14 17:07:09 +0900

    20170614 jquery 수정 (스케줄 등록)

--- src/main/java/com/daou/moyeo/schedule/service/ScheduleServiceImpl.java
+++ src/main/java/com/daou/moyeo/schedule/service/ScheduleServiceImpl.java
@@ -1,5 +1,7 @@
 package com.daou.moyeo.schedule.service;
 
+import java.util.List;
+
 import org.mybatis.spring.support.SqlSessionDaoSupport;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,11 @@ public class ScheduleServiceImpl extends SqlSessionDaoSupport implements Schedul
 	@Override
 	public int insertScheduleInfo(ScheduleDTO scheduleDto) {
 		return getSqlSession().insert("schedule.insertScheduleInfo", scheduleDto);
+	}
+
+	@Override
+	public List<Object> selectScheduleList(int memberNo) {
+		return getSqlSession().selectList("schedule.selectScheduleList", memberNo);
 	}
 	
 }
